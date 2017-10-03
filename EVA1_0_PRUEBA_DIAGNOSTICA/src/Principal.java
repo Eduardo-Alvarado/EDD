fa94b07c4ff5fc65ca930b22c88afc7ba4b49164
Eduardo-Alvarado <32476565+Eduardo-Alvarado@users.noreply.github.com>
2017-10-03 00:19:58 -0600

    Add files via upload

---
+++ EVA1_0_PRUEBA_DIAGNOSTICA/src/Principal.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author administrador
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    int x;
+    
+    public int solicitarNumero(){
+        int x;
+        x.
+        
+        if(x = 1){
+            System.out.println("Enero");
+        }
+        if(x = 2){
+            System.out.println("Febrero");
+        }
+        if(x = 3){
+            System.out.println("Marzo");
+        }
+        if(x = 4){
+            System.out.println("Abril");
+        }
+        if(x = 5){
+            System.out.println("Mayo");
+        }
+        if(x = 6){
+            System.out.println("Junio");
+        }
+        if(x = 7){
+            System.out.println("Julio");
+        }
+        if(x = 8){
+            System.out.println("Agosto");
+        }
+        if(x = 9){
+            System.out.println("Septiembre");
+        }
+    }
+            
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
